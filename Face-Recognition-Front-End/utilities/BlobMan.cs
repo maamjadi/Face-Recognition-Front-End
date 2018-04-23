7043237b3801b6c008dbbbeea563aa2b7225dea0
Unknown <alialsaeedi19@gmail.com>
2018-04-24 00:12:19 +0200

    preparing the api,s for storage and face detection

---
+++ Face-Recognition-Front-End/utilities/BlobMan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+namespace FaceRecognitionFrontEnd.utilities
+{
+    public class BlobMan
+    {
+        CloudBlobContainer _recofaContainer;
+        public static BlobMan Instance { get; } = new BlobMan();
+
+        BlobMan()
+        {
+            _recofaContainer = _blobClient.GetContainerReference("recfa");
+
+
+        }
+        const string connectionString = "DefaultEndpointsProtocol=https;AccountName=alialsaeedi19;AccountKey=gy6yyV+QS3X88zNElVugIxIR7YqwVdDNb1jidgnuFBBdV5hXq+gzw8CBtKF14BgRxhO1BUtK5evheqJ6OTensg==;EndpointSuffix=core.windows.net";
+        CloudBlobClient _blobClient = CloudStorageAccount.Parse(connectionString).CreateCloudBlobClient();
+
+        public async Task<List<Uri>> GetAllBlobUris(string localPath)
+        {
+            var contToken = new BlobContinuationToken();
+            var allBlobs = await _recofaContainer.ListBlobsSegmentedAsync(contToken).ConfigureAwait(false);
+            var uris = allBlobs.Results.Select(b => b.Uri).ToList();
+
+            return uris;
+        }
+
+        public async Task<string> UploadFileAsync(string localPath, Stream image)
+        {
+            var uniqueBlobName = Guid.NewGuid().ToString();
+            uniqueBlobName += Path.GetExtension(localPath);
+
+            var blobRef = _recofaContainer.GetBlockBlobReference(uniqueBlobName);
+
+            await blobRef.UploadFromStreamAsync(image).ConfigureAwait(false);
+            return blobRef.Uri.AbsoluteUri;
+                   
+        }
+       
+    }
+}
---
+++ Face-Recognition-Front-End/utilities/RecMan.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FaceRecognitionFrontEnd;
+using Xamarin.Cognitive.Face;
+using Microsoft.ProjectOxford.Face;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using System.Linq;
+
+namespace FaceRecognitionFrontEnd.utilities
+{
+    public class RecMan
+    {
+        public static RecMan Instance { get; } = new RecMan();
+
+        RecMan()
+        {
+        }
+        public static  async Task RegisterStudents(List<Student> students, string personGroupId)
+        {
+            if (students.Count > 0)
+            {
+                var faceServiceClient = new FaceServiceClient(Constants.FaceApiKey, Constants.FaceEndpoint);
+
+                // Step 1 - Create Person Group
+                var personGroup = Guid.NewGuid().ToString();
+                await faceServiceClient.CreatePersonGroupAsync(personGroupId, "Xamarin Employees");
+
+                // Step 2 - Add persons (and faces) to person group.
+                foreach (var student in students)
+                {
+                    // Step 2a - Create a new person, identified by their name.
+                    var p = await faceServiceClient.CreatePersonAsync(personGroupId, student.UserName);
+                    // Step 3a - Add a face for that person.
+                    await faceServiceClient.AddPersonFaceAsync(personGroupId, p.PersonId, student.PhotoURL);
+                }
+
+                // Step 3 - Train facial recognition model.
+                await faceServiceClient.TrainPersonGroupAsync(personGroupId);
+            }
+        }
+
+        public static async Task<string> ExecuteFindSimilarFaceCommandAsync(string personGroupId, MediaFile photo)
+        {
+            var stream = photo.GetStream();
+
+
+            var faceServiceClient = new FaceServiceClient(Constants.FaceApiKey, Constants.FaceEndpoint);
+                // Step 4a - Detect the faces in this photo.
+                var faces = await faceServiceClient.DetectAsync(stream);
+                var faceIds = faces.Select(face => face.FaceId).ToArray();
+
+                if (faceIds.Length == 0)
+                return null;
+                            
+                // Step 4b - Identify the person in the photo, based on the face.
+                var results = await faceServiceClient.IdentifyAsync(personGroupId, faceIds);
+                var result = results[0].Candidates[0].PersonId;
+
+                // Step 4c - Fetch the person from the PersonId and display their name.
+                var person = await faceServiceClient.GetPersonAsync(personGroupId, result);
+                return person.Name;
+          
+            
+        }
+    }
+}
